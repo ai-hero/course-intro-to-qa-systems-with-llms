384c365c7513c339e46a12e2a0ef2ca3005ef353
Rahul Parundekar <rparundekar@gmail.com>
2023-09-02 12:24:44 -0700

    Hello ChatGPT

--- tests/test_hello_chatgpt.py
+++ tests/test_hello_chatgpt.py
@@ -4,6 +4,8 @@ from introduction.hello_chatgpt import get_response
 
 def test_greet() -> None:
     """Test greet() function."""
-    assert get_response(
-        [{"role": "user", "content": "Hello, my name is Rahul."}], stream=False
-    ), "Unable to test openai.ChatCompletion.create()"
+    response = get_response([{"role": "user", "content": "Hello, my name is Rahul."}], stream=False)
+    message = response.choices[0]["message"]  # type: ignore[union-attr]
+    assert "role" in message, "Response should have a role."
+    assert message["role"] == "assistant", "Response should have assistant role."
+    assert "content" in message, "Response should have content."
